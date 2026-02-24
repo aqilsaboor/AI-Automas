d0897a0e23c4556ea38ec4049b83e147ac801375
githubzohaib <zohaibaliwork@gmail.com>
2026-02-24 22:58:49 +0500

    Add app.listen for port listening

--- server/index.js
+++ server/index.js
@@ -9,4 +9,10 @@ connectDB();
 
 app.get("/", (req, res) => res.send("Backend is running!"));
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
 module.exports = app;
