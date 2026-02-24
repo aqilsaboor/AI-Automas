16c967c6205165aa88e44252e25d346df19d9a81
githubSubhanKhan <subbikhan124@gmail.com>
2026-02-25 03:11:00 +0500

    added all fixed

--- client/src/components/layouts/Form.tsx
+++ client/src/components/layouts/Form.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+
 export default function ContactForm() {
   const [formData, setFormData] = useState({
     name: '',
